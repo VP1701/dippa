23c243fe2257c4de0b67252a45a86ac8df3eba51
Veeti <veeti.pekonen@gmail.com>
2026-07-03 17:04:10 +0300

    ..

--- ros2_ws/src/safe_control/safe_control/safety_filter.py
+++ ros2_ws/src/safe_control/safe_control/safety_filter.py
@@ -15,16 +15,23 @@ from acados_template import AcadosModel
 
 class CbfMpcSolver:
 
-    def __init__(self, L_f, L_r, r_dics, dt, h, gamma_cbf, n_obs_max):
+    def __init__(self, L_f, L_r, r_dics, dt, h, gamma_cbf, n_obs_max, gamma_max, h_slack=1e4):
 
 
         self.L_f = L_f
         self.L_r = L_r
         self.r_dics = r_dics
+
         self.dt = dt
         self.h = h
         self.gamma_cbf = gamma_cbf
-        self.n_obs_max = n_obs_max
+
+        self.gamma_max = gamma_max # Maximum center link angle
+        self.n_obs_max = n_obs_max # maximum amount of allowed obstacles
+
+        self.margin = margin # safety margin
+
+        self.h_slack = h_slack
 
 
         self.model, self.sym = self._build_model()
@@ -96,6 +103,7 @@ class CbfMpcSolver:
     def _build_ocp(self, ):
         """
         Build the optimal control problem for the cbf mpc
+
         """
 
         x = self.sym["x"]
@@ -109,29 +117,63 @@ class CbfMpcSolver:
         # Initialize the optimal control problem caontainer
         ocp = AcadosOcp()
         ocp.model = self.model
-        ocp.dims.N = self.H
+        ocp.dims.N = self.h
 
         # tracking cost
         ocp.cost.cost_type = 'NONLINEAR_LS'
         ocp.cost.cost_type_e = 'NONLINEAR_LS'
-        ocp.mode.cost_y_expr = u - u_ref
-        ocp.mode.cost_y_expr_e = ca.vertcat(x[3]) 
+        ocp.model.cost_y_expr = u - u_ref
+        ocp.model.cost_y_expr_e = ca.vertcat(x[3]) 
+
+        # desired reference is 0 since we desire minimally invasive controls
         ocp.cost.yref = np.zeros(2)
         ocp.cost.yref_e = np.zeros(1)
+
+        # Weights
         ocp.cost.W = np.diag([q_v, q_w])
         ocp.cost.W_e = np.diag([W_terminal])
 
         # bounds
-        ocp.constraints.idxbu = np.array([0, 1])
-        ocp.constraints.lbu = np.array([-self.v_max, -self.omega_max])
-        ocp.constraints.ubu = np.array([self.v_max, self.omega_max])
-
+        ocp.constraints.idxbu = np.array([0, 1]) # Indices for box constraints of u
+        ocp.constraints.lbu = np.array([-self.v_max, -self.omega_max]) # lowerbounds of u
+        ocp.constraints.ubu = np.array([self.v_max, self.omega_max]) # Upperbounds of u
+        ocp.constraints.idxbx = np.array([3]) # Indices for box constraints of x
+        ocp.constraints.lbx = np.array([-self.gamma_max]) # lowerbounds of x
+        ocp.constraints.ubx = np.array([self.gamma_max]) # Upperbounds of x
 
         
         # CBF constraints
 
+        h_terms = []
 
+        for disc in self.sym["discs"]:
+            for i in range(self.n_obs_max):
+                ox, oy = obs_cord[2 * i], obs_cord[2 * i + 1]
+                active = obs_cord_active[i]
+                disc_x = disc[0]
+                disc_y = disc[1]
+                # Distance to obstacle from disc
+                dist = ca.sqrt((disc_x - ox) ** 2 + (disc_y - oy) ** 2 + 1e-9) # small slack to keep square root non zero
+                h = dist - self.r_dics - self.margin
+                h_terms.append(h + (1.0 - active) * self.h_slack) # if active append h if not append h largenumber
 
+        h_expr = ca.vertcat(*h_terms)
+
+        # Set ocp nonlinear constraint
+        ocp.model.con_h_expr = h_expr
+
+        n_h = h_expr.shape[0]
+
+        # set cosntraint for h function
+        ocp.constraints.lh = np.zeros(n_h)
+        ocp.constraints.uh = np.full(n_h, 1e6) # no upper bound but acados needs some value -> bignumber
+
+        # adding quadratic slack violation weights
+        ocp.constraints.idxsh = np.arrange(n_h) # all constraints get slacks
+        opt.cost.ZL = np.full(n_h, self.slack_weight)
+        opt.cost.ZU = np.full(n_h, self.slack_weight)
+        opt.cost.Zl = np.full(n_h, self.slack_weight)
+        opt.cost.Zu = np.full(n_h, self.slack_weight)
 
 
 
